2e916044ac7a00cb57c6ef211d70333b8e726746
saiicodes <bernardsaii20@gmail.com>
2022-07-23 17:54:09 +0000

    !: added the react app directory and made more changes to updateREADME.js

--- update-README.js
+++ update-README.js
@@ -10,6 +10,8 @@ function readWriteAsync() {
   2. Run \`npm install\`
   3. Run \`npm test\`
 
+  __NOTE: The react app for the project is in the\`client\` directory__
+
   ## Project information
 
   A CI/CD workflow which will produce a screenshot of your homepage and keep it updated as you keep changing the code. This kind of always-up-to-date screenshot can be useful for your README or as the basis of marketing materials that include screenshots of your website.
